599165c3109c46e11adab593df39a18358e12a12
Sahithi Boddupalli (Student) <sboddupa@sundevils.asu.edu>
2022-02-21 00:16:49 -0700

    Code to pass test cases of Decrypt

--- Cipher.py
+++ Cipher.py
@@ -15,4 +15,17 @@ class Cipher(object):
         return res
         
     def decrypt(self, string, shift):
-        pass
+        res = ''
+        for char in string: 
+            if char == ' ':
+                res+= char
+            elif char.islower():
+                res+= chr((ord(char) - shift - 97) % 26 + 97)
+                print(ord(char), ord(char)-shift-97, (ord(char) - shift - 97)%26, (ord(char) - shift - 97) % 26 + 97)
+            elif  char.isupper():
+                res+= chr((ord(char) - shift - 65) % 26 + 65)
+            elif char.isdigit():
+                res+= char
+            else:
+                res+= char
+        return res
